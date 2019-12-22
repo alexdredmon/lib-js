440f4655af397609decbab00931f810f7b941e18
Alex Redmon <alex.redmon@cylera.com>
2019-12-22 13:39:12 -0500

    Common  utils and actions/middlewares/reducers/selectors for API, flash messages, and dialogs

---
+++ core/util/api.js
@@ -0,0 +1,66 @@
+import { APP_NAMESPACE } from 'config'
+import { toUrlParams } from './url'
+
+export class Api {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl
+  }
+
+  getToken = () => {
+    if (! this.token) {
+      const sessionToken = window.localStorage.getItem(`${APP_NAMESPACE}:api.token`)
+      if (sessionToken) {
+        this.token = sessionToken
+      }
+    }
+    return this.token
+  }
+
+  getAuthHeaders = () => {
+    const token = this.getToken()
+    if (! token) {
+      return {}
+    }
+    return {
+      'Authorization': `Token ${token}`,
+    }
+  }
+
+  handleError(error, callback) {
+    console.log(`Error: ${error}`)
+    callback(error)
+  }
+
+  handleResponse(response, callback) {
+    return response.json()
+      .then(json => callback && callback(json))
+      .catch(error => this.handleError(error, callback))
+  }
+
+  get(path, params = {}, callback = () => {}) {
+    const url = `${this.baseUrl}/${path}?${toUrlParams(params)}`
+    return fetch(url, {
+      headers: {
+        ...this.getAuthHeaders(),
+      }
+    }).then(
+      response => this.handleResponse(response, callback)
+    )
+  }
+
+  post(path, data, callback = () => {}) {
+    const url = `${this.baseUrl}/${path}`
+    return fetch(url, {
+      method: "POST",
+      headers: {
+        ...this.getAuthHeaders(),
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(data)
+    }).then(
+      response => this.handleResponse(response, callback)
+    )
+  }
+}
+
+export default Api
